ab069aa3a740438384d120cedac2d600eb69701e
AlexxalexS <37020080+AlexxalexS@users.noreply.github.com>
2021-10-20 00:12:41 +0300

    Initial Commit

---
+++ testCamera/testCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  testCameraApp.swift
+//  testCamera
+//
+//  Created by Alexey on 20.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct testCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
